f0ce9aa640307b2f9423b54351d22697f487af62
Chuck729 <alexaca@rose-hulman.edu>
2016-03-22 15:16:56 -0400

    Test card test passes abstract was removed from card to get test to work for now

---
+++ RHFYP/TestCard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RHFYP
+{
+    public class TestCard :Card
+    {
+        //Create test card with certain values
+        //this is just a test with random assigned values
+        //each other card implemented will have meaningful values
+        public TestCard()
+        {
+            CardCost = 3;
+            Name = "TestCard";
+            Description = "This card is used for testing purposes";
+            Type = "action";
+        }
+    }
+}
