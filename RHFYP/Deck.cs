00c48821c3b95444a1c2970438c7d8300c050617
Chuck729 <alexaca@rose-hulman.edu>
2016-03-23 15:09:07 -0400

    Added basic implementations to deck

--- RHFYP/Deck.cs
+++ RHFYP/Deck.cs
@@ -6,9 +6,88 @@ using System.Threading.Tasks;
 
 namespace RHFYP
 {
-    class Deck
+    class Deck : IDeck
     {
         // TODO: Need a WasDeckChanged() method
         // TODO: Need a List<Card> LookAtDeck() method
+
+        public List<Card> cards { get; set; }
+        public void AddCard(Card card)
+        {
+            cards.Add(card);
+        }
+
+        public IDeck AppendDeck(IDeck deck)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CardCount()
+        {
+           return cards.Capacity;
+        }
+
+        public ICollection<Card> Cards()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CountCardType(string type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Card DrawCard()
+        {
+            if(cards.Count == 0)
+            {
+                //do something
+            }
+
+            int index = cards.Count - 1;
+            Card c = cards[index];
+            cards.RemoveAt(index);
+            return c;
+        }
+
+        public ICollection<Card> DrawCards(int n)
+        {
+            List<Card> nextCards = new List<Card>();
+
+            for (int x = 0; x < n; x++)
+            {
+                nextCards.Add(DrawCard());
+            }
+            return nextCards;
+        }
+
+        public Card GetFirstCard(Predicate<Card> pred)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool InDeck(Card card)
+        {
+           return cards.Contains(card);
+        }
+
+        public void Shuffle()
+        {
+
+        }
+        public void ShuffleIn(ICollection<Card> otherCards)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDeck SubDeck(Predicate<Card> pred)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool WasDeckChanged()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
