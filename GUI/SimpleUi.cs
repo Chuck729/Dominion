f39dc90620140eb32cc5befd56a3792e1cbd4723
Christian-Nunnally <nunnalcs@rose-hulman.edu>
2016-03-24 09:42:52 -0400

    Realized I was working in master

--- GUI/SimpleUi.cs
+++ GUI/SimpleUi.cs
@@ -1,21 +1,12 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace GUI
 {
-    public interface ISimpleUi
+    class SimpleUi
     {
-        /// <summary>
-        /// If the user clicks a Ui the mouse coords should be sent to each sub Ui.
-        /// Results from the click (if any events happen) should be added to an output stack in the Ui.
-        /// </summary>
-        /// <param name="x">Mouse click X pos</param>
-        /// <param name="y">Mouse click Y pos</param>
-        void SendClick(int x, int y);
-
-        /// <summary>
-        /// Draws this Ui onto the <see cref="Graphics"/> object.
-        /// </summary>
-        /// <param name="g">The <see cref="Graphics"/> object to draw on.</param>
-        void Draw(Graphics g);
     }
 }
