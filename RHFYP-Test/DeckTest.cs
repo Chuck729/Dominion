1994cd0031988e6816cad8d1e2a200d981f92cf8
wlucas7474 <weierlj@rose-hulman.edu>
2016-03-23 20:12:53 -0400

    Added TestDrawCards

--- RHFYP-Test/DeckTest.cs
+++ RHFYP-Test/DeckTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using RHFYP;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace RHFYP_Test
 {
@@ -22,17 +23,43 @@ namespace RHFYP_Test
         {
             Deck deck = new Deck();
             Card rose = new Rose();
-            Card hippyCamp = new HippieCamp();
+            Card hippieCamp = new HippieCamp();
             Card purdue = new Purdue();
             deck.AddCard(rose);
-            deck.AddCard(hippyCamp);
+            deck.AddCard(hippieCamp);
             deck.AddCard(purdue);
 
             Assert.AreEqual(rose, deck.DrawCard());
-            Assert.AreEqual(hippyCamp, deck.DrawCard());
+            Assert.AreEqual(hippieCamp, deck.DrawCard());
             Assert.AreEqual(purdue, deck.DrawCard());
             Assert.AreEqual("Out of cards, need to reshuffle", deck.DrawCard());
 
+        }
+
+        [TestMethod]
+        public void TestDrawCards()
+        {
+            Deck deck = new Deck();
+            Card rose = new Rose();
+            Card hippieCamp = new HippieCamp();
+            Card purdue = new Purdue();
+
+            deck.AddCard(rose);
+            deck.AddCard(hippieCamp);
+            deck.AddCard(purdue);
+
+            List<Card> drawTwo = new List<Card>();
+            drawTwo.Add(rose);
+            drawTwo.Add(hippieCamp);
+
+            Assert.AreEqual(drawTwo, deck.DrawCards(2));
+
+            List<Card> drawOne = new List<Card>();
+            drawOne.Add(purdue);
+
+            Assert.AreEqual(drawOne, deck.DrawCards(1));
+
+            Assert.AreEqual("Out of cards, need to reshuffle", deck.DrawCards(4));
         }
     }
 }
