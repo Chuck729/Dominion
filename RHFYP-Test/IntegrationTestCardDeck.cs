be53f3bd42bc5043b6e7570505ee717dc3d9eeae
hauges <hauges@rose-hulman.edu>
2016-03-24 23:07:45 -0400

    TestGetCardDataFromDeck integration test passes

---
+++ RHFYP-Test/IntegrationTestCardDeck.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RHFYP;
+
+namespace RHFYP_Test
+{
+    [TestClass]
+    public class IntegrationTestCardDeck
+    {
+        [TestMethod]
+        public void TestGetCardDataFromDeck()
+        {
+            Deck d = new Deck();
+            d.AddCard(new Rose());
+            Assert.AreSame("victory", d.DrawCard().Type);
+        }
+    }
+}
