43d2d3295bf3b6098db7fb2f2a6b5916223acbe1
Christian-Nunnally <nunnalcs@rose-hulman.edu>
2016-03-30 15:09:54 -0400

    Added temp buy menu

--- GUI/BuyDeckUi.cs
+++ GUI/BuyDeckUi.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using RHFYP;
 using RHFYP.Cards;
 
@@ -7,9 +9,9 @@ namespace GUI
 {
     public class BuyDeckUi : SimpleUi
     {
-        private const int AnimationFrames = 30;
+        private const int AnimationFrames = 45;
 
-        private List<BuyCardViewer> _buyCardViewers = new List<BuyCardViewer>(); 
+        private List<BuyCardViewer> _buyCardViewers = new List<BuyCardViewer>();
 
         private Point _mouseLocation = Point.Empty;
         private bool _mouseIn;
@@ -17,9 +19,25 @@ namespace GUI
 
         private int _animationFrame;
 
+        /// <summary>
+        /// Returns what it thinks the lowest displayed card value was (+ the width of the last card)
+        /// Should reset to 0 when SetBuyDeck() is called.
+        /// </summary>
+        private int _lazyBiggestY;
+
         private bool _isCardItemMousedOver;
         private Card _cardItemMousedOver;
         private Card _cardItemSelected;
+
+        /// <summary>
+        /// Is the buy menu fully collapsed.
+        /// </summary>
+        private bool Collapsed => _animationFrame <= 0;
+
+        /// <summary>
+        /// Is the buy menu fully expanded.
+        /// </summary>
+        private bool Expanded => _animationFrame >= AnimationFrames;
 
         public override bool SendClick(int x, int y)
         {
@@ -39,17 +57,71 @@ namespace GUI
         /// <param name="g">The <see cref="Graphics"/> object to draw on.</param>
         public override void Draw(Graphics g)
         {
+
             ChangeAnimationFrame();
+
             var bufferGraphics = Graphics.FromImage(BufferImage);
+            bufferGraphics.SmoothingMode = SmoothingMode.HighQuality;
+
+            bufferGraphics.Clear(Color.FromArgb(0, 0, 0, 0));
+
+            // Background and top corner line for testing.
+            //            bufferGraphics.FillRectangle(Brushes.Black, 0, 0, Width, Height);
+            //            bufferGraphics.DrawLine(Pens.Green, Point.Empty, new Point(40, 40));
 
             foreach (var cardViewer in _buyCardViewers)
             {
+                CalculatePixelLocationForAnimation(cardViewer);
                 cardViewer.DrawCardViewer(bufferGraphics);
+                _lazyBiggestY = Math.Max(cardViewer.PixelLocation.Y, _lazyBiggestY);
+                if (_animationFrame == 0) break;
             }
 
             // Draw the buffered image onto the main graphics object.
-            g.DrawImage(BufferImage, Point.Empty);
+            g.DrawImage(BufferImage, Location);
             base.Draw(g);
+        }
+
+        public void CalculatePixelLocationForAnimation(BuyCardViewer bcv)
+        {
+            var widthAndMargin = BuyCardViewer.CircleRectangle.Width + BuyCardViewer.MarginBetweenCircles;
+            var xMin = Width - widthAndMargin;
+            var xMax = Width - (bcv.GridLocation.X + 1) * widthAndMargin;
+
+            var yMax = (bcv.GridLocation.Y * widthAndMargin) + BuyCardViewer.MarginBetweenCircles;
+            var yMin = BuyCardViewer.MarginBetweenCircles;
+
+            if (!Collapsed && _lazyBiggestY > Height)
+            {
+                var adjustedMouseY = _mouseLocation.Y - BuyCardViewer.MarginBetweenCircles;
+                var adjustedHeight = Height - 2* BuyCardViewer.MarginBetweenCircles;
+
+                var yOverflow = _lazyBiggestY - Height;
+                var adjustedMouseYPrecent = (float)adjustedMouseY / adjustedHeight;
+                var offset = (int)(adjustedMouseYPrecent * yOverflow);
+
+                yMin -= offset;
+                yMax -= offset;
+            }
+
+            var pixelX = EaseOutQuint(_animationFrame, xMin, xMax - xMin, AnimationFrames);
+            var pixelY = EaseOutQuint(_animationFrame, yMin, yMax - yMin, AnimationFrames);
+            bcv.PixelLocation = new Point((int)pixelX, (int)pixelY);
+        }
+
+        /// <summary>
+        /// Formula aquired from http://gizma.com/easing/#quint2
+        /// </summary>
+        /// <param name="time">The current time or the current frame.</param>
+        /// <param name="start">The start point of the animation.</param>
+        /// <param name="change">The total distance that should be animated across.</param>
+        /// <param name="duration">The duration of the animation or the number of frames.</param>
+        /// <returns>The position along the animation.</returns>
+        private float EaseOutQuint(float time, float start, float change, float duration)
+        {
+            time /= duration;
+            time--;
+            return change * (time * time * time * time * time + 1) + start;
         }
 
         private void ChangeAnimationFrame()
@@ -78,9 +150,9 @@ namespace GUI
         /// <returns>True is the mouse event is consitered "swallowed"</returns>
         public override bool SendMouseLocation(int x, int y)
         {
-            if (x >= (Width - BuyCardViewer.CircleRectangle.Width - (MarginBetweenCircles * 2)) && x <= BufferImage.Width)
+            if (x >= (Width - BuyCardViewer.CircleRectangle.Width - (BuyCardViewer.MarginBetweenCircles * 2)) && x <= BufferImage.Width)
             {
-                if (y >= 0 && y <= BuyCardViewer.CircleRectangle.Width + (MarginBetweenCircles*2))
+                if (y >= 0 && y <= BuyCardViewer.CircleRectangle.Width + (BuyCardViewer.MarginBetweenCircles * 2))
                 {
                     _mouseIn = true;
                 }
@@ -95,14 +167,48 @@ namespace GUI
             return base.SendMouseLocation(x, y);
         }
 
-        private Point CalculateCirclePosition(int circleX, int circleY)
+        /// <summary>
+        /// This method needs to be called at the start of the game to set the deck of cards
+        /// the game will be using.  Passing in this deck gives the UI what it needs to draw the
+        /// animated side bar.
+        /// </summary>
+        /// <param name="buyDeck"></param>
+        public void SetBuyDeck(IDeck buyDeck)
         {
-            return Point.Empty;
+            _lazyBiggestY = 0;
+            var gridSizeX = 2;
+            var gridSizeY = 2;
+
+            // TODO: Unfinished
+            var i = 0;
+            foreach (var card in buyDeck.Cards())
+            {
+                _buyCardViewers.Add(new BuyCardViewer(0, i));
+                i++;
+            }
+            i = 0;
+            foreach (var card in buyDeck.Cards())
+            {
+                _buyCardViewers.Add(new BuyCardViewer(1, i));
+                i++;
+            }
+
+
+            var bitmapWidth = gridSizeX * (BuyCardViewer.CircleRectangle.Width + BuyCardViewer.MarginBetweenCircles);
+
+            BufferImage = new Bitmap(bitmapWidth, ParentUi.Height);
+
+            if (ParentUi == null)
+            {
+                throw new InvalidOperationException("A BuyDeckUi has to be a child of another ISimple Ui.");
+            }
+            Location = new Point(ParentUi.Width - bitmapWidth, 0);
         }
 
-        public void SetBuyDeck(Deck buyDeck)
+        public void AdjustSizeAndPosition(int parentWidth, int parentHeight)
         {
-            
+            BufferImage = new Bitmap(BufferImage.Width, parentHeight);
+            Location = new Point(parentWidth - BufferImage.Width, 0);
         }
     }
 }
