fa50129ca2a7a5870ffb208875a5a2b643903ae3
Chuck729 <alexaca@rose-hulman.edu>
2016-03-31 22:59:33 -0400

    committing icard and igame

---
+++ RHFYP/ICard.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RHFYP.Cards
+{
+    public interface ICard
+    {
+        int CardCost { get; }
+        /// <summary>
+        /// The string that represents the title of the card.
+        /// </summary>
+        /// <remarks>
+        /// This is what determines what image is displayed for this card.  
+        /// This string must match the title of the resource (eg. "grass" or "corperation")
+        /// </remarks>
+        string Name { get; }
+
+        // The type of the card ("action", "victory", "treasure")
+        string Type { get; }
+
+        // The description of what the card does when played
+        string Description { get; }
+
+        //The amount of victory points each card is worth
+        int VictoryPoints { get; }
+
+        Point Location { get; set; }
+
+
+        bool IsAddable { get; set; }
+
+        
+        void PlayCard(Player player);
+    }
+}
---
+++ RHFYP/IGame.cs
@@ -0,0 +1,27 @@
+﻿using RHFYP.Cards;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RHFYP
+{
+    //This class will control the interactions between the players and cards and handle all piles of cards available to buy
+    public interface IGame
+    {
+        /// <summary>
+        /// populates decks of the 10 action cards, 3 treasure cards, and 6 victory cards for the game
+        /// </summary>
+        void generateCards();
+
+        /// <summary>
+        /// method called when a card is bought and will take a card out of the deck passed in by the parameter
+        /// </summary>
+        /// <param name="pile"></param>
+        ICard buyCard(IDeck pile, IPlayer player);
+
+
+        
+    }
+}
