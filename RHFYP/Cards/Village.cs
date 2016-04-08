e050ac320b9279b20e89dd97150620db59f078f0
Christian-Nunnally <nunnalcs@rose-hulman.edu>
2016-04-08 02:48:19 -0400

    added village

---
+++ RHFYP/Cards/Village.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RHFYP.Cards
+{
+    public class Village : Card
+    {
+        public Village() : base(3, "Village", "+1 civilian +2 managers", "action", 0, "village")
+        {
+        }
+
+        public override void PlayCard(Player player)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
