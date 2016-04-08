2c22bfd3c080cdf3132d0b471f478b9c480ce465
Christian-Nunnally <nunnalcs@rose-hulman.edu>
2016-04-08 02:44:49 -0400

    Added wall stree (chancellor)

---
+++ RHFYP/Cards/WallStreet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RHFYP.Cards
+{
+    public class WallStreet : Card // Chancellor
+    {
+        public WallStreet() : base(3, "Wall Street", "+2 coins.  All civilians visit the same places on your next turn", "action", 0, "")
+        {
+        }
+
+        public override void PlayCard(Player player)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
