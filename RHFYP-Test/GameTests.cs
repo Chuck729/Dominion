cd830fbf29e3df32c1e538345ac13d428cc49a0f
Christian-Nunnally <nunnalcs@rose-hulman.edu>
2016-04-08 01:13:22 -0400

    GenerateCards_TresureCardsAlwaysPresent_10FamilyBusinessesInBuyDeck() fails

--- RHFYP-Test/GameTests.cs
+++ RHFYP-Test/GameTests.cs
@@ -5,19 +5,42 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RHFYP;
+using RHFYP.Cards;
 
 namespace RHFYP_Test
 {
+
     [TestClass]
     public class GameTests
     {
+
         [TestMethod]
         public void GenerateCards_CardIsPutIntoBuyDeck_BuyDeckNotEmpty()
         {
+
             var g  = new Game();
+
             Assert.IsTrue(g.BuyDeck.CardList.Count == 0);
             g.generateCards();
             Assert.IsTrue(g.BuyDeck.CardList.Count > 0);
+
+        }
+
+        [TestMethod]
+        public void GenerateCards_TresureCardsAlwaysPresent_10FamilyBusinessesInBuyDeck()
+        {
+
+            var g = new Game();
+
+            Assert.IsTrue(g.BuyDeck.CardList.Count == 0);
+            g.generateCards();
+            Assert.IsTrue(g.BuyDeck.SubDeck(IsFamilyBusiness).CardList.Count == 10);
+
+        }
+
+        public bool IsFamilyBusiness(ICard card)
+        {
+            return (card.Name.ToLower().Equals("familybusiness"));
         }
     }
 }
