e0b1bc0e6bc1310dcc85578991abcc593f5c1d8f
Christian-Nunnally <nunnalcs@rose-hulman.edu>
2016-04-08 02:40:38 -0400

    Added military base (moat)

--- RHFYP/Cards/Area51.cs
+++ RHFYP/Cards/Area51.cs
@@ -2,7 +2,7 @@
 
 namespace RHFYP.Cards
 {
-    class Area51 : Card // Chapel
+    public class Area51 : Card // Chapel
     {
         public Area51() : base(2, "Area 51", "Nuke up to 4 tiles on the map", "action", 0, "")
         {
