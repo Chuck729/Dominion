c1cc4ec30589dafd7266031d8ed870bc2fea3f17
Christian-Nunnally <nunnalcs@rose-hulman.edu>
2016-04-08 01:58:22 -0400

    GenerateCards_VictoryCardsPresent_8Mits() fails

--- RHFYP-Test/GameTests.cs
+++ RHFYP-Test/GameTests.cs
@@ -75,6 +75,18 @@ namespace RHFYP_Test
         }
 
         [TestMethod]
+        public void GenerateCards_VictoryCardsPresent_8Mits()
+        {
+
+            var g = new Game();
+
+            Assert.IsTrue(g.BuyDeck.CardList.Count == 0);
+            g.GenerateCards();
+            Assert.IsTrue(g.BuyDeck.SubDeck(IsMit).CardList.Count == 8);
+
+        }
+
+        [TestMethod]
         public void GenerateCards_HippieCampCardsPresent_CorrectNumberOfCurses()
         {
 
