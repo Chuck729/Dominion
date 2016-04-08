5c500a80675d355ddca59fa42d2bedb4a8af58d4
Christian-Nunnally <nunnalcs@rose-hulman.edu>
2016-04-08 02:52:34 -0400

    Added construction site (woodcutter)

---
+++ RHFYP/Cards/ConstructionSite.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RHFYP.Cards
+{
+    public class ConstructionSite : Card // Workshop
+    {
+        public ConstructionSite() : base(3, "Construction Site", "Transform into a tile costing up to 4", "action", 0, "")
+        {
+        }
+
+        public override void PlayCard(Player player)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
