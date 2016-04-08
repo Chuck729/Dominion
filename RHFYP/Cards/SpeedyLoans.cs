537a5f38461d59685971462b36130e3eb89bb3e1
Christian-Nunnally <nunnalcs@rose-hulman.edu>
2016-04-08 03:12:42 -0400

    added speedy loans (money lender)

---
+++ RHFYP/Cards/SpeedyLoans.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RHFYP.Cards
+{
+    public class SpeedyLoans : Card // Money lender
+    {
+        public SpeedyLoans() : base(4, "Speedy Loans", "Destory a small business and +3 coins.", "action", 0, "speedyloans")
+        {
+        }
+
+        public override void PlayCard(Player player)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
