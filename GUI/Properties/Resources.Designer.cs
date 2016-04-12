4aa90e4068957b48845c07092e999679584ed055
Christian-Nunnally <nunnalcs@rose-hulman.edu>
2016-04-12 12:43:55 -0400

    Added area 51 graphic

--- GUI/Properties/Resources.Designer.cs
+++ GUI/Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace GUI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap area51 {
+            get {
+                object obj = ResourceManager.GetObject("area51", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap buysymbol {
             get {
                 object obj = ResourceManager.GetObject("buysymbol", resourceCulture);
