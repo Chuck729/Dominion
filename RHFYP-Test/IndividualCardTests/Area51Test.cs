cb40678784298a872581abeef06650fe1057c985
wlucas7474 <weierlj@rose-hulman.edu>
2016-04-20 21:33:35 -0400

    Touched up Area51
    Began Area51Tests, fixed Area51 description, and added Docs for Area51
    PlayCard()

---
+++ RHFYP-Test/IndividualCardTests/Area51Test.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RHFYP;
+using RHFYP.Cards;
+using Rhino.Mocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RHFYP_Test.IndividualCardTests
+{
+    [TestClass]
+    class Area51Test
+    {
+        private MockRepository _mocks;
+
+        [TestInitialize()]
+        public void Initialize()
+        {
+            _mocks = new MockRepository();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException), 
+            "Player doesn't exist.")]
+        public void TestArea51PlayerNotExist()
+        {
+            Card c = new Area51();
+            c.PlayCard(null);
+        }
+
+        [TestMethod]
+        public void TestArea51OneCardTrashed()
+        {
+            Card c = new Area51();
+            Player p = _mocks.StrictMock<Player>();
+
+        }
+
+    }
+}
