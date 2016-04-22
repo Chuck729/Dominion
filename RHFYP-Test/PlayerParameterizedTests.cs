96f9195b3fa6d7420b94608cd97645218ef2eb55
wlucas7474 <weierlj@rose-hulman.edu>
2016-04-22 12:09:38 -0400

    Attempted parameterized testing for Player

---
+++ RHFYP-Test/PlayerParameterizedTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RHFYP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RHFYP_Test
+{
+    [TestClass]
+    public class PlayerParameterizedTests
+    {
+        //private int _inputNumber;
+        //private Player p;
+
+
+        //[TestInitialize]
+        //public void Initialize()
+        //{
+        //    p = new Player("test");
+        //}
+
+        //public PlayerParameterizedTests(int input)
+        //{
+        //    this._inputNumber = input;
+        //}
+
+        //[TestMethod]
+        //public void TestCallAddGold()
+        //{
+        //    TestAddGold(3, 3);
+        //}
+
+        //public void TestAddGold(int gold, int expected)
+        //{
+        //    p.AddGold(gold);
+        //    Assert.AreEqual(p.Gold, expected);
+        //}
+    }
+}
