e85f94f9f4f19eba16fdabf6d5ec9d460d36fc9a
Christian-Nunnally <nunnalcs@rose-hulman.edu>
2016-04-27 11:01:19 -0400

    Added game over feature scenario

---
+++ RHFYP-Test/Features/Features/GameOverFeature.feature
@@ -0,0 +1,10 @@
+﻿Feature: GameOverFeature
+	When the game should end
+	And what should happen when it's over
+
+@mytag
+Scenario: AllRoseHulmansBought
+	Given I have a game
+	And there are no Rose-Hulman cards left in the buy deck
+	Then The game should end
+	And The player with the most victory points should win
---
+++ RHFYP-Test/Features/Steps/GameOverFeatureSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace RHFYP_Test
+{
+    [Binding]
+    public class GameOverFeatureSteps
+    {
+        [Given(@"I have a game")]
+        public void GivenIHaveAGame()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"there are no Rose-Hulman cards left in the buy deck")]
+        public void GivenThereAreNoRose_HulmanCardsLeftInTheBuyDeck()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"The game should end")]
+        public void ThenTheGameShouldEnd()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"The player with the most victory points should win")]
+        public void ThenThePlayerWithTheMostVictoryPointsShouldWin()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
