0e402123cabd06235ea5851fe022fcba0041f5cf
hauges <hauges@rose-hulman.edu>
2016-04-28 11:40:21 -0400

    TestSmallBusinessGoldIncrease mock fixed

--- RHFYP-Test/IndividualCardTests/SmallBusinessTest.cs
+++ RHFYP-Test/IndividualCardTests/SmallBusinessTest.cs
@@ -40,13 +40,11 @@ namespace RHFYP_Test.IndividualCardTests
 
             using (_mocks.Ordered())
             {
-
+                p.AddGold(1);
             }
             _mocks.ReplayAll();
 
             c.PlayCard(p);
-
-            Assert.AreEqual(5, p.Gold);
 
             _mocks.VerifyAll();
         }
