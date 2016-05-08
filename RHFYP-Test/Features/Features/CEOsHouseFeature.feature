93b1b381d17f773d6e87751e9487a90544e1722d
hauges <hauges@rose-hulman.edu>
2016-05-08 18:53:34 -0400

    PlayCEOsHouse fails

---
+++ RHFYP-Test/Features/Features/CEOsHouseFeature.feature
@@ -0,0 +1,14 @@
+﻿Feature: CEOsHouseFeature
+	As a player, you should be able to play a 
+	CEOsHouse card
+
+@CEOsHouse
+Scenario: Play CEOsHouse
+	Given I have a game
+	And the game has 2 players
+	And player 0 has 0 gold
+	And player 0 has a CEOsHouse in their hand
+	And player 0 has a Company in their hand
+	When player 0 plays the CEOsHouse card
+	And player 0 plays the Company card
+	Then player 0 has 4 gold
---
+++ RHFYP-Test/Features/Steps/CEOsHouseFeatureSteps.cs
@@ -0,0 +1,43 @@
+﻿using RHFYP.Cards;
+using RHFYP.Cards.ActionCards;
+using RHFYP.Cards.TreasureCards;
+using RHFYP_Test.Features.Steps;
+using System;
+using TechTalk.SpecFlow;
+
+namespace RHFYP_Test
+{
+    [Binding]
+    public class CEOsHouseFeatureSteps
+    {
+
+        private readonly GameSteps _game;
+        private readonly SpeedyLoansPlayCardSteps _speedyLoansSteps; // used to see if Exception is thrown
+
+        public CEOsHouseFeatureSteps(GameSteps game, SpeedyLoansPlayCardSteps speadyLoansSteps)
+        {
+            _game = game;
+            _speedyLoansSteps = speadyLoansSteps;
+        }
+
+        private ICard _cCard = new CeosHouse();
+        [Given(@"player (.) has a CEOsHouse in their hand")]
+        public void GivenPlayerHasACEOsHouseInTheirHand(int p0)
+        {
+            _game.Game.Players[p0].Hand.AddCard(_cCard);
+        }
+        
+        [When(@"player (.) plays the CEOsHouse card")]
+        public void WhenPlayerPlaysTheCEOsHouseCard(int p0)
+        {
+            _game.Game.Players[p0].PlayCard(_cCard);
+        }
+
+        [When(@"player (.) plays the Company card")]
+        public void WhenPlayerPlaysTheCompanyCard(int p0)
+        {
+            _game.Game.Players[p0].PlayCard(new Company());
+        }
+
+    }
+}
